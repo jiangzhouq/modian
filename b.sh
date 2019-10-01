857040836851b56f29bb8c3fdeb883d2ce53dfc0
jiangzhouq <jiangzhouq@gmail.com>
2019-10-02 02:23:37 +0800

    switch to sc.ftqq.com message sender

--- b.sh
+++ b.sh
@@ -5,14 +5,13 @@ do
 	((a++))
 	echo -e "\n"
 	echo "---------######正在进行第 $a 次尝试。`date '+%Y-%m-%d %H:%M:%S'`"
-	https_proxy=https://localhost:1080 scrapy crawl biu
-	https_proxy=https://localhost:1080 scrapy crawl fangao
-	https_proxy=https://localhost:1080 scrapy crawl huoguo
-	https_proxy=https://localhost:1080 scrapy crawl niao
-	https_proxy=https://localhost:1080 scrapy crawl zhu
-	https_proxy=https://localhost:1080 scrapy crawl zuipao
-	https_proxy=https://localhost:1080 scrapy crawl bayinhe
-	https_proxy=https://localhost:1080 scrapy crawl tuzi
+	scrapy crawl biu
+	scrapy crawl fangao
+	scrapy crawl huoguo
+	scrapy crawl moyu
+	scrapy crawl niao
+	scrapy crawl shengzhe
+	scrapy crawl tuzi
 	for b in `seq -w 30 -1 0`
 	do
 		echo -en "---------将会在$b秒后重试。\r"  
--- modian/spiders/shengzhe.py
+++ modian/spiders/shengzhe.py
@@ -1,18 +1,18 @@
 # -*- coding=utf-8 -*-
 
 import scrapy
-import telegram 
 import time
 import logging
+import requests
 
 logger = logging.getLogger(__name__)
 class modianSpider(scrapy.Spider):
-    name = "zhu"
+    name = "shengzhe"
     allowed_domains = ["modian.com"]
     #项目ID
-    product_id = 80986
+    product_id = 79038
     #支持项ID，Like：reward-9
-    item_id = [6]
+    item_id = [3]
     
     product_url = "https://zhongchou.modian.com/product_rewards/index?pid=" + str(product_id) + "&_mpos=pro_goback_web"
     start_urls = [
@@ -20,7 +20,8 @@ class modianSpider(scrapy.Spider):
     ] 
     
     def parse(self, response):
-        bot = telegram.Bot(token='823530794:AAGk2v80sSgevtLMivvLcwJnRvrGiWsb0ns')
+        wxurl = "https://sc.ftqq.com/SCU63006T23f45cef2f5637767bb29fbd42f9f8c65d907671c95c8.send"
+
         print '---------' + '\033[46m' + response.xpath('//h1/text()').extract()[0].encode("UTF-8") + '\033[0m'
         project_name = response.xpath('//h1/text()').extract()[0].encode("UTF-8")
         for sel_item_id in self.item_id:
@@ -30,19 +31,6 @@ class modianSpider(scrapy.Spider):
             if "已抢光" in item_div.xpath('.//p[@class="n"][1]/text()[1]').extract()[0].encode("UTF-8"):
                 print '---------' + item_name
             else:
-                print '---YES---' + item_name 
-                bot.sendMessage(chat_id=791147649, text=project_name + "\n" + item_name)
-            
-            
-#        print('\033[1;31;40m')
-#        if len(list) == 0:
-#               print '无此项，请检查。'
-#        else:
-#            if "已抢光" in list[0].encode("UTF-8"):
-#                print list[0]
-#            else:
-#                print 'result : bitch you are here!!!!!!!!!!!!!!!'
-#                bot.sendMessage(chat_id=791147649, text="马拉开波448有货")
-#        print bot.getMe()
-#        print('\033[0m')
-        
+                print '---YES---' + item_name
+                wxmessage = {"text":project_name, "desp":item_name}
+                requests.get(wxurl, params=wxmessage)
